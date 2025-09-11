eb7d0de0b8b3f7a6d665497d630135b8c47f9fc8
Neha Yadav <ny663922@gmail.com>
2025-09-11 05:45:06 +0530

    learing basics

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ const combine = require("./combine-file")
 
 console.log(combine);
 console.log("Hello Neha, Node.js is working!");
+const arr = [1,8,9,5,3,3,4,2,5,5,6]
+   let result= arr.filter((item)=>{
+    return item===5;
+    
+})
+console.warn(result);
