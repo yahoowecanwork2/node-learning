e125122ec11db8e9b381df054aa320118c9ff4f3
Neha Yadav <ny663922@gmail.com>
2025-09-12 06:07:34 +0530

    learing about package.json

--- making-basic-server.js
+++ making-basic-server.js
@@ -1,5 +1,5 @@
 const http = require('http');
 http.createServer((req,resp)=>{
-    resp.write("hey! this is Neha");
+    resp.write(" <h1>hey! this is Neha</h1>");
     resp.end()
-}).listen(4500);
+}).listen(4400);
