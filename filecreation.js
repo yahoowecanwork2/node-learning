567637be93e29d21bed9991cde160d503ed67265
Neha Yadav <ny663922@gmail.com>
2025-12-08 16:42:03 +0530

    learing about file system how to readdile data

--- filecreation.js
+++ filecreation.js
@@ -1,2 +1,17 @@
-const fs=require('fs');
-fs.writeFileSync("hello.txt","heyyy!!!");
+const { error, log } = require("console");
+const fs = require("fs");
+// fs.writeFileSync("test.txt", "heyyy!!!");
+// readfile
+// const fs = require("fs");
+
+// fs.writeFileSync("test.txt", "heyyy!!!");
+
+// readFile
+fs.readFile("hello.txt", "utf8", (error, data) => {
+  if (error) {
+    console.log("❌ ERROR FOUND:");
+    console.log(error.message);
+  } else {
+    console.log("File Data:", data);
+  }
+});
