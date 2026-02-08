9bf25f22d9ed16465fe6961cef2aaa2f0482f6f9
Neha Yadav <ny663922@gmail.com>
2026-02-08 11:09:57 +0530

    basics

---
+++ section-1/basics.js
@@ -0,0 +1,17 @@
+let a = 10;
+var b = 20;
+const c = 200;
+
+a = 100;
+b = 200;
+// c = 300;
+console.log(a + b + c);
+if (a === 100) {
+  console.log("this is if condition");
+} else {
+  console.log("this is else condition");
+}
+const fruit = (item) => {
+  console.log("this is " + item);
+};
+console.log("apple");
